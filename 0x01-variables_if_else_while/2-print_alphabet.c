9bae668a595d94602b9ee3f9f2141b55dd739697
monicahwamuhu <monicahwamuhu2@gmail.com>
2022-09-09 16:44:51 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entrypoint
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
 
