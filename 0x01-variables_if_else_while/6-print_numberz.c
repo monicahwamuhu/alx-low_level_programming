1800b2847b4732b040366d5126230180c26a907f
monicahwamuhu <monicahwamuhu2@gmail.com>
2022-09-09 18:01:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *  main - prints all single digits of base 10 followed by new line
+ *
+ *  Return- return 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = '0' ; digit <= '9' ; digit++)
+
+	{ 
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
+
